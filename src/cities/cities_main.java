75d734398bdb5dd60cc27ba3e20a69417da3f0d0
ddthj <mwirsing@rthighschool.org>
2014-10-14 20:12:03 -0400

    Created base for game
    created cities_m, the object for cities_main
    created startup & shutdown scripts
    added basic rendering abilities
    deleted Jgame.java

--- src/cities/cities_main.java
+++ src/cities/cities_main.java
@@ -1,8 +1,78 @@
 package cities;
 
-public class cities_main {
-	public static void main(String args[]){
-		System.out.println("Hello World");
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
+
+public class cities_main extends Canvas implements Runnable{
+
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 250, HEIGHT = 250, SCALE = 2;
+	public static boolean running = false;
+	public Thread citiesThread;
+	public synchronized void start(){
+		if(running)return;
+		running = true;
+		citiesThread = new Thread(this);
+		citiesThread.start();
 	}
+	public synchronized void stop(){
+		if(!running)return;
+		running = false;
+		try {
+			citiesThread.join();
+		} catch (InterruptedException e) {e.printStackTrace();}
+	}
+	
+	public void tick() {
+		
+	}
+	public void render(){
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null){
+			createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		g.fillRect(0, 0, WIDTH * SCALE, HEIGHT * SCALE);
+		g.dispose();
+		bs.show();
+	}
+	public void run() {
+		long lastTime = System.nanoTime();
+		final double amountOfTicks = 60D;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now-lastTime) / ns;
+			lastTime = now;
+			if(delta>=1){
+				tick();
+				delta--;
+			}
+			render();
+		}
+		stop();
+	}
+
+	public static void main(String args[]){
+		cities_main cities_m = new cities_main();
+		cities_m.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		cities_m.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		cities_m.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		
+		JFrame frame = new JFrame("HungryCities");
+		frame.setSize(WIDTH * SCALE, HEIGHT * SCALE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.add(cities_m);
+		frame.setVisible(true);
+	}
+		
 
 }
