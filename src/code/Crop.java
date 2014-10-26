9536a48793c3f620f2fe83eac29b08981358d86c
ddthj <mwirsing@rthighschool.org>
2014-10-26 14:17:43 -0400

    I CAN JAVA
    AAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHH
    
    got map generator working correctly, very basic map. working on a better
    generator though

--- src/code/Crop.java
+++ src/code/Crop.java
@@ -9,8 +9,7 @@ public class Crop {
 		this.spritesheet = spritesheet;
 		grass = crop(spritesheet,0,0,500,500);
 		mud = crop(spritesheet,1,0,500,500);
-		trees = crop(spritesheet,2,0,500,500);
-		scrap = crop(spritesheet,3,0,500,500);		
+		scrap = crop(spritesheet,2,0,500,500);		
 	}
 	private BufferedImage crop(BufferedImage spritesheet,int col,int row,int w,int h){
 		BufferedImage result = spritesheet.getSubimage(col*120,row*120,w,h);
--- src/code/Map.java
+++ src/code/Map.java
@@ -1,12 +1,48 @@
 package code;
 
+import java.util.Random;
+
 public class Map {
+	private int insert;
+	public void printmap(int[][] gamemap,int MAPSIZE){
+		for(int i=0;i<MAPSIZE;i++){
+			for(int j=0;j<MAPSIZE;j++){
+				System.out.println(gamemap[i][j]);
+			}
+			//System.out.println();
+		}
+	}
 
 	public void gen(int MAPSIZE){
-		int[][] gamemap = new int[MAPSIZE^2][MAPSIZE^2];
-		for(int i=0;i<gamemap.length;i++){
-			
+		int[][] gamemap = new int[MAPSIZE][MAPSIZE];
+		int a = 0;
+		Random rand = new Random();
+		for(int x = 0;x<MAPSIZE;x++){
+			for(int y=0;y<MAPSIZE;y++){
+				insert = rand.nextInt((3-1)+1)+1;
+				if(insert==3){
+					insert += rand.nextInt((350-100)+1)+100;
+				}
+				gamemap[x][y] = insert;				
+			}
 		}
+		printmap(gamemap,MAPSIZE);
+		/*for(int i=0;i < MAPSIZE;i++){
+			insert = rand.nextInt((3-1)+1)+1;
+			if(insert > 2){
+				insert += rand.nextInt((399-100)+1)+100;
+			}
+			gamemap[i][a] = insert;
+			if(i==MAPSIZE){
+				if (a==MAPSIZE){
+					break;
+				}
+				i = 0;
+				a++;
+				
+			}*/
+			
+		
 		
 		
 	}
